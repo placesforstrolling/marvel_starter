d80f8a63c333158ef9475db0260bfb43eb6dae30
Strange12 <mustafaanvarov@mail.ru>
2021-11-15 16:44:20 +0500

    6_115 Added list of characters and random button

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -15,9 +15,13 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
@@ -34,6 +38,10 @@ class RandomChar extends Component {
         });
     }
 
+    onRandomClick = () => {
+        this.updateChar();
+    }
+
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
@@ -41,7 +49,7 @@ class RandomChar extends Component {
             .then(this.onCharLoaded)
             .catch(this.onError);
     }
-
+    
    render () {
         const {char, loading, error} = this.state;
         const errorMessage = error ? <ErrorMessage/> : null;
@@ -61,7 +69,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.onRandomClick}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -73,9 +81,10 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+
     return (
             <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} style={thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? {objectFit: 'contain'} : null} alt="Random character" className="randomchar__img"/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
